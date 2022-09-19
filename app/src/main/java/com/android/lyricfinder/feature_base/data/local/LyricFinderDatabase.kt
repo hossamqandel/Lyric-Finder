a372a4edb64f986222c4defe03cc2f7f3d8caca2
Hossam <hossamegyqandel@gmail.com>
2022-09-19 22:27:15 +0200

    prepare room database

---
+++ app/src/main/java/com/android/lyricfinder/feature_base/data/local/LyricFinderDatabase.kt
@@ -0,0 +1,8 @@
+package com.android.lyricfinder.feature_base.data.local
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(entities = [], version = 1)
+abstract class LyricFinderDatabase : RoomDatabase(){
+}
