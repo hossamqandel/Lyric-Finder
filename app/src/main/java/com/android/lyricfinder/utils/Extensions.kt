a75335e1fd8872f1812a4f5a020e3f8aa8c19941
Hossam <hossamegyqandel@gmail.com>
2022-09-19 19:56:22 +0200

    Add show toast extension function

--- app/src/main/java/com/android/lyricfinder/utils/Extensions.kt
+++ app/src/main/java/com/android/lyricfinder/utils/Extensions.kt
@@ -1,2 +1,8 @@
 package com.android.lyricfinder.utils
 
+import android.content.Context
+import android.widget.Toast
+
+fun Context.showToast(message: String){
+    Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+}
