4018ff09a9e8cf8ebd797dd1051855d551302940
Hossam <hossamegyqandel@gmail.com>
2022-09-19 23:42:40 +0200

    database environment implemented

--- app/src/main/java/com/android/lyricfinder/feature_base/data/local/LyricFinderDatabase.kt
+++ app/src/main/java/com/android/lyricfinder/feature_base/data/local/LyricFinderDatabase.kt
@@ -2,7 +2,13 @@ package com.android.lyricfinder.feature_base.data.local
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.android.lyricfinder.feature_lyric_detail.data.local.entity.DetailEntity
+import com.android.lyricfinder.feature_search.data.local.entity.SearchEntity
 
-@Database(entities = [], version = 1)
+
+@Database(entities = [SearchEntity::class, DetailEntity::class], version = 1)
 abstract class LyricFinderDatabase : RoomDatabase(){
+
+    val DATABASE_NAME = "lyric_finder"
+
 }
