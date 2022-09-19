f759fb3db17848a0b0763a96cf221d7802ed58f2
Hossam <hossamegyqandel@gmail.com>
2022-09-19 22:26:32 +0200

    Setup remote response dto data classes

---
+++ app/src/main/java/com/android/lyricfinder/feature_lyric_detail/data/remote/dto/DetailDto.kt
@@ -0,0 +1,79 @@
+package com.android.lyricfinder.feature_lyric_detail.data.remote
+
+import com.google.gson.annotations.SerializedName
+
+data class DetailDto(
+    val meta: Meta,
+    val response: Response
+)
+
+data class Meta(
+    val status: Int
+)
+
+data class Response(
+    val lyrics: Lyrics
+)
+
+data class Lyrics(
+    val _type: String,
+    val api_path: String,
+    val lyrics: LyricsX,
+    val path: String,
+    val song_id: Int,
+    val tracking_data: TrackingData
+)
+
+data class LyricsX(
+    val body: Body
+)
+
+data class TrackingData(
+    val annotatable_id: Int,
+    val annotatable_type: String,
+    val cohort_ids: List<Int>,
+    val comment_count: Int,
+    val created_at: String,
+    val created_month: String,
+    val created_year: Int,
+    val featured_video: Boolean,
+    val has_apple_match: Boolean,
+    val has_description: Boolean,
+    val has_featured_annotation: Boolean,
+    val has_recirculated_articles: Boolean,
+    val has_recommendations: Boolean,
+    @SerializedName("has_translation_q&a")
+    val has_translation_qa: Boolean,
+    val has_verified_callout: Boolean,
+    val has_youtube_url: Boolean,
+    val hot: Boolean,
+    val lyrics_language: String,
+    @SerializedName("music?")
+    val music: Boolean,
+    val nrm_target_date: String,
+    val nrm_tier: Int,
+    val platform_variant: String,
+    val primary_album: String,
+    val primary_album_id: Int,
+    val primary_artist: String,
+    val primary_artist_id: Int,
+    val primary_tag: String,
+    val primary_tag_id: Int,
+    val release_date: String,
+    val song_id: Int,
+    val song_tier: String,
+    val tag: String,
+    val title: String,
+    val web_interstitial_variant: String
+)
+
+data class Body(
+    val dom: Dom,
+    val html: String,
+    val plain: String
+)
+
+data class Dom(
+    val children: List<Any>,
+    val tag: String
+)
---
+++ app/src/main/java/com/android/lyricfinder/feature_search/data/remote/dto/SearchDto.kt
@@ -0,0 +1,93 @@
+package com.android.lyricfinder.feature_search.data.remote.dto
+
+data class SearchDto(
+    val meta: Meta,
+    val response: Response
+)
+
+data class Meta(
+    val status: Int
+)
+
+data class Response(
+    val hits: List<Hit>
+)
+
+data class Hit(
+    val highlights: List<Any>,
+    val index: String,
+    val result: Result,
+    val type: String
+)
+
+data class Result(
+    val _type: String,
+    val annotation_count: Int,
+    val api_path: String,
+    val artist_names: String,
+    val featured_artists: List<FeaturedArtist>,
+    val full_title: String,
+    val header_image_thumbnail_url: String,
+    val header_image_url: String,
+    val id: Int,
+    val instrumental: Boolean,
+    val language: String,
+    val lyrics_owner_id: Int,
+    val lyrics_state: String,
+    val lyrics_updated_at: Int,
+    val path: String,
+    val primary_artist: PrimaryArtist,
+    val pyongs_count: Int,
+    val relationships_index_url: String,
+    val release_date_components: ReleaseDateComponents,
+    val release_date_for_display: String,
+    val song_art_image_thumbnail_url: String,
+    val song_art_image_url: String,
+    val stats: Stats,
+    val title: String,
+    val title_with_featured: String,
+    val updated_by_human_at: Int,
+    val url: String
+)
+
+data class FeaturedArtist(
+    val _type: String,
+    val api_path: String,
+    val header_image_url: String,
+    val id: Int,
+    val image_url: String,
+    val index_character: String,
+    val iq: Int,
+    val is_meme_verified: Boolean,
+    val is_verified: Boolean,
+    val name: String,
+    val slug: String,
+    val url: String
+)
+
+data class PrimaryArtist(
+    val _type: String,
+    val api_path: String,
+    val header_image_url: String,
+    val id: Int,
+    val image_url: String,
+    val index_character: String,
+    val iq: Int,
+    val is_meme_verified: Boolean,
+    val is_verified: Boolean,
+    val name: String,
+    val slug: String,
+    val url: String
+)
+
+data class ReleaseDateComponents(
+    val day: Int,
+    val month: Int,
+    val year: Int
+)
+
+data class Stats(
+    val hot: Boolean,
+    val pageviews: Int,
+    val unreviewed_annotations: Int
+)
