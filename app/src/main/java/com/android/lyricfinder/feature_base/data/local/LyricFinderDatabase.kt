294f49a410e945183d9c51186decce08496a4a2d
Hossam <hossamegyqandel@gmail.com>
2022-09-20 03:43:14 +0200

    add some implementation

--- app/src/main/java/com/android/lyricfinder/feature_base/data/local/LyricFinderDatabase.kt
+++ app/src/main/java/com/android/lyricfinder/feature_base/data/local/LyricFinderDatabase.kt
@@ -5,10 +5,10 @@ import androidx.room.RoomDatabase
 import com.android.lyricfinder.feature_lyric_detail.data.local.entity.DetailEntity
 import com.android.lyricfinder.feature_search.data.local.entity.SearchEntity
 
+private const val DATABASE_NAME = "lyric_finder"
 
 @Database(entities = [SearchEntity::class, DetailEntity::class], version = 1)
 abstract class LyricFinderDatabase : RoomDatabase(){
 
-    val DATABASE_NAME = "lyric_finder"
 
 }
---
+++ app/src/main/java/com/android/lyricfinder/feature_search/presentation/SearchFragment.kt
@@ -0,0 +1,36 @@
+package com.android.lyricfinder.feature_search.presentation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.android.lyricfinder.R
+import com.android.lyricfinder.databinding.FragmentSearchBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class SearchFragment : Fragment() {
+
+    private var _binding: FragmentSearchBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentSearchBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+
+}
